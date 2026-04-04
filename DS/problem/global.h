34958257a7e008b07e38e8ab9108b0fa16fc6ecf
Easyhyz <h2546419945@163.com>
2026-04-04 14:01:18 +0800

    global.h 是全局头文件，方便以后直接引用 一元多项式求导很简单

---
+++ DS/problem/global.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <math.h>
+
+#define TRUE 1
+#define FALSE 0
+#define OK 1
+#define ERROR 0
+#define INITSIZE 100
+
+typedef int Elemtype;
---
+++ DS/problem/一元多项式求导.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <math.h>
+
+#define TRUE 1
+#define FALSE 0
+#define OK 1
+#define ERROR 0
+#define INITSIZE 100
+
+typedef int Elemtype;
+
+struct Term
+{
+    int coef;
+    int exp;
+}terms[1000], results[1000];
+
+
+
+int main()
+{
+    int n = 0;
+    while (scanf("%d %d", &terms[n].coef, &terms[n].exp) == 2)
+    {
+        if (terms[n].coef == 0 || terms[n].exp == 0)
+        {
+            break;
+        }
+        n++;
+    }
+
+    int m = 0;
+    for (int i = 0; i < n; i++)
+    {
+        results[m].coef = terms[m].coef * terms[m].exp;
+        results[m].exp = terms[m].exp - 1;
+        m++;
+    }
+    if (m == 0)
+    {
+        printf("0 0");
+    }
+    else
+    {
+        for (int i = 0; i < m; i++)
+        {
+            printf("%d %d", results[i].coef, results[i].exp);
+            if (i < m-1) printf(" ");
+        }
+        
+    }
+
+    return 0;
+}
